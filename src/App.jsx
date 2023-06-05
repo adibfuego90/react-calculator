9bce719227b5a3ac54ea2ee7cb983bd9b58149a3
adibfuego90 <adiburraman11@gmail.com>
2023-06-05 08:29:17 +0600

    react clock

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import React from "react";
+import { useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  let time = new Date().toLocaleTimeString();
+  const [ctime, setCtime] = useState(time);
+
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  };
+  setInterval(updateTime, 1000);
+
+  return (
+    <div>
+      <>
+        <h1>{ctime}</h1>
+      </>
+    </div>
+  );
 };
 
 export default App;
